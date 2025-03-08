6da8e6dd614c0b84a9b7ad7e9cdb291422966fe0
luanAfons0 <luan.hmatao@gmail.com>
2025-03-08 18:19:56 -0300

    update: better env treatment

--- src/config/env.ts
+++
@@ -1,8 +0,0 @@
-import "dotenv/config";
-
-const ENV = {
-  PORT: process.env.PORT,
-  DATABASE_URL: process.env.DATABASE_URL,
-};
-
-export default ENV;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import ENV from "./config/env.js";
+import "dotenv/config";
 import app from "./app.js";
 
-app.listen(ENV.PORT ?? 3000, () =>
+app.listen(process.env.PORT ?? 3000, () =>
   console.log(`Server is up and running on port ${3000}!`)
 );
